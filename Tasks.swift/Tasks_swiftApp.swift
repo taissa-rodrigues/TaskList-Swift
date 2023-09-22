01995ca98d6188b68a83ecec508456d35f86788d
Bernado Santos <bernardosmm2004@gmail.com>
2023-09-22 13:54:57 -0300

    Initial Commit

---
+++ Tasks.swift/Tasks_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tasks_swiftApp.swift
+//  Tasks.swift
+//
+//  Created by User on 22/09/23.
+//
+
+import SwiftUI
+
+@main
+struct Tasks_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
